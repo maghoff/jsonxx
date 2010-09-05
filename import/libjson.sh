83ada5c2f966501bdd06d34cd7847ab0e4f4592d
maghoff <unknown>
2010-09-05 13:00:03 +0200

    Froze the libjson version to 0.8.

--- import/libjson.sh
+++ import/libjson.sh
@@ -2,5 +2,10 @@
 
 SOURCE=http://github.com/vincenthz/libjson.git
 TARGET=libjson
+TAG=v0.8
 
-git clone "$SOURCE" "$TARGET"
+git clone "$SOURCE" "$TARGET" &&
+
+pushd "$TARGET" &&
+git checkout "$TAG" 2>/dev/null &&
+popd
