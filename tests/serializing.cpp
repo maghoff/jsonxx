8977de88bf50da23bceec9b4b856a30087a74d03
maghoff <unknown>
2010-09-15 23:45:49 -0700

    Generalized deserializer a bit

--- tests/serializing.cpp
+++ tests/serializing.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <map>
 #include <sstream>
+#include <stdexcept>
 #include "test_util.hpp"
 #include "compact_writer.hpp"
 #include "parser.hpp"
@@ -13,6 +15,26 @@ struct simple_struct {
 
 class simple_struct_object_listener : public jsonxx::object_listener {
     simple_struct& s;
+
+    union field_pointer {
+        std::string simple_struct::* t_string;
+        int simple_struct::* t_int;
+        double simple_struct::* t_double;
+        bool simple_struct::* t_bool;
+    } target_field;
+
+    enum expect_type {
+        t_ignore,
+        t_string,
+        t_int,
+        t_double,
+        t_bool
+    } expected_type;
+
+    std::map<std::string, field_pointer> field_from_key;
+    std::map<std::string, expect_type> type_from_key;
+
+    void match(expect_type e);
 
 public:
     simple_struct_object_listener(simple_struct&);
@@ -36,15 +58,22 @@ public:
 simple_struct_object_listener::simple_struct_object_listener(simple_struct& s_) :
     s(s_)
 {
+    #define REGISTER(field, type) \
+        field_from_key[#field].type = &simple_struct::field; \
+        type_from_key[#field] = type;
 
+    REGISTER(name, t_string);
+    REGISTER(value, t_int);
+
+    #undef REGISTER
 }
 
 simple_struct_object_listener::~simple_struct_object_listener() {
-
 }
 
-void simple_struct_object_listener::key(const std::string&) {
-
+void simple_struct_object_listener::key(const std::string& k) {
+    target_field = field_from_key[k];
+    expected_type = type_from_key[k];
 }
 
 void simple_struct_object_listener::start_object() { }
@@ -53,8 +82,22 @@ void simple_struct_object_listener::end_object() { }
 void simple_struct_object_listener::start_array() { }
 void simple_struct_object_listener::end_array() { }
 
-void simple_struct_object_listener::value(const std::string& v) { s.name = v; }
-void simple_struct_object_listener::value(int v) { s.value = v; }
+void simple_struct_object_listener::match(expect_type e) {
+    if (expected_type != e) {
+        throw std::runtime_error("Actual type did not match expected type when deserializing");
+    }
+}
+
+void simple_struct_object_listener::value(const std::string& v) {
+    match(t_string);
+    &s->*target_field.t_string = v;
+}
+
+void simple_struct_object_listener::value(int v) {
+    match(t_int);
+    &s->*target_field.t_int = v;
+}
+
 void simple_struct_object_listener::value(double) { }
 void simple_struct_object_listener::value(jsonxx::bool_type) { }
 void simple_struct_object_listener::value(jsonxx::null_type) { }
@@ -103,5 +146,3 @@ bool serializing_tests() {
 
     return ok;
 }
-
-
