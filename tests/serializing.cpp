ee7777e1a1c03f76b784bcbbbde31f9537b308b5
maghoff <unknown>
2010-09-16 05:14:01 -0700

    Added a generic simple serializer

--- tests/serializing.cpp
+++ tests/serializing.cpp
@@ -34,6 +34,27 @@ namespace {
 
 typedef jsonxx::deserializer<simple_struct> simple_struct_deserializer;
 
+template <class T>
+void simple_serializer(jsonxx::object_listener& o, const T& t) {
+    typename jsonxx::type_information<T>::info_map_t &info_map = jsonxx::type_information<T>::info_map;
+    typedef typename jsonxx::type_information<T>::info_map_t::const_iterator c_iter;
+    c_iter begin = info_map.begin(), end = info_map.end();
+
+    o.start_object();
+    for (c_iter i = begin; i != end; ++i) {
+        o.key(i->first);
+        if (i->second.type == jsonxx::field_type::t_string) o.value(t.*i->second.field.t_string);
+        else if (i->second.type == jsonxx::field_type::t_int) o.value(t.*i->second.field.t_int);
+        else if (i->second.type == jsonxx::field_type::t_double) o.value(t.*i->second.field.t_double);
+        else if (i->second.type == jsonxx::field_type::t_bool) o.value(t.*i->second.field.t_bool);
+        else {
+            assert(false);
+            o.value(jsonxx::null);
+        }
+    }
+    o.end_object();
+}
+
 bool serialize_simple_struct() {
     bool ok = true;
 
@@ -42,10 +63,7 @@ bool serialize_simple_struct() {
     std::stringstream ss;
     jsonxx::compact_writer cw(ss);
 
-    cw.start_object();
-    cw.pair("name", s.name);
-    cw.pair("value", s.value);
-    cw.end_object();
+    simple_serializer(cw, s);
 
     CHECK_EQUAL(ss.str(), "{\"name\":\"the name\",\"value\":42}");
 
